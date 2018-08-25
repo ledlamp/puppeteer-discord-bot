9e6ee2cf5400a0f988df09fc0fb23d6aee335b69
Lamp <ledlamp89@gmail.com>
2018-08-24 23:32:55 -0700

    Next level

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
 (async () => {
-  var puppeteer = require('puppeteer');
-  var browser = await puppeteer.launch();
-  
-  var Discord = require('discord.js');
-  var bot = new Discord.Client();
-  bot.login("NDc4NzA4NzI1MTAwODM4OTUw.DmDa-A.g3iiuER3Tt_aeF9YUDmEB-ZurFU");
+	var fs = require("fs");
+	try {
+		var puppeteer = require('puppeteer-core');
+	} catch(e) {
+		var puppeteer = require('puppeteer');
+	}
+	var browser = await puppeteer.launch({executablePath: fs.existsSync("/usr/bin/chromium-browser") ? "/usr/bin/chromium-browser" : undefined});
 
-  bot.on("message", async function(message){
-    if (message.content.toLowerCase().startsWith("!gi")) {
-      let page = await browser.newPage();
-      await page.setViewport({width: 1440, height: 900});
-      await page.goto(`https://www.google.com/search?q=${encodeURIComponent(message.content.substr(4))}&tbm=isch`)
-      let screenshot = await page.screenshot({type: 'png'});
-      message.channel.send(new Discord.Attachment(screenshot, "screenshot.png"));
-      await page.close();
-    }
-  });
+	var Discord = require('discord.js');
+	var bot = new Discord.Client();
+	bot.login(fs.readFileSync('./token.txt','utf8'));
+
+	var cmdPrefix = "p!";
+	bot.on("message", async function(msg){
+		if (!msg.content.startsWith(cmdPrefix)) return;
+
+		var args = msg.content.split(" ");
+		var cmd = args[0].slice(cmdPrefix.length).toLowerCase();
+		var query = args.slice(1).join(" ");
+
+		switch (cmd) {
+			case "screenshot":
+			case "ss":
+				pup(msg, query);
+				break;
+			case "google":
+			case "g":
+				pup(msg, `https://www.google.com/search?q=${encodeURIComponent(query)}`);
+				break;
+			case "google-images":
+			case "gi":
+				pup(msg, `https://www.google.com/search?q=${encodeURIComponent(query)}&tbm=isch`);
+				break;
+			case "bing":
+			case "b":
+				pup(msg, `https://www.bing.com/search?q=${encodeURIComponent(query)}`);
+				break;
+			case "bing-images":
+			case "bi":
+				pup(msg, `https://www.bing.com/images/search?q=${encodeURIComponent(query)}`);
+				break;
+			case "youtube":
+			case "yt":
+				pup(msg, `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`);
+				break;
+/*
+			case "":
+			case "":
+				pup(msg, ``);
+				break;
+*/
+		}
+
+
+	});
+
+	async function pup(message, url) {
+		var page = await browser.newPage();
+		await page.setViewport({width: 1440, height: 900});
+		await page.goto(url);
+		var screenshot = await page.screenshot({type: 'png'});
+		message.channel.send(new Discord.Attachment(screenshot, "screenshot.png"));
+		page.close();
+	}
 
 })();
