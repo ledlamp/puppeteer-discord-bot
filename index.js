bd448a7504b1fe95d4b68c3929012c1505aa15a5
Lamp <ledlamp89@gmail.com>
2019-06-21 15:39:43 -0700

    convert to discord.io use less ram plus some refactor

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 console.log("start");
 (async () => {
 	var fs = require("fs");
-	try {
-		var puppeteer = require('puppeteer-core');
-	} catch(e) {
-		var puppeteer = require('puppeteer');
-	}
-	var browser = await puppeteer.launch({executablePath: fs.existsSync("/usr/bin/chromium-browser") ? "/usr/bin/chromium-browser" : undefined});
+	var puppeteer = require('puppeteer');
+	var browser = await puppeteer.launch();
 
-	var Discord = require('discord.js');
-	var bot = new Discord.Client();
-	bot.login(fs.readFileSync('./token.txt','utf8').trim());
-	bot.on('ready', ()=>{
-		bot.user.setActivity("p!help")
+	var Discord = require('discord.io');
+	var bot = new Discord.Client({
+		token: fs.readFileSync('token.txt','utf8').trim(),
+		autorun: true
 	});
+	bot.setPresence({game: "p!help"});
 
-	var cmdPrefix = "p!";
-	bot.on("message", async function(msg){
-		if (!msg.content.startsWith(cmdPrefix)) return;
+	bot.on("message", async function(userName, userID, channelID, message, event){
+		if (!message.startsWith("p!")) return;
 
-		console.log(`[${new Date().toLocaleString()}] [${msg.guild && msg.guild.name}] [${msg.channel.name}] ${msg.author.tag} invoked command: ${msg.content}`);
+		console.log(`[${new Date().toLocaleString()}] [${event.d.guild_id}(${client.servers[event.d.guild_id]&&client.servers[event.d.guild_id].name})] [${channelID}(#${client.channels[channelID]&&client.channels[channelID].name})] User ${userID} (${userName}#${event.d.author.discriminator}) invoked command: ${message}`);
 
-		var args = msg.content.split(" ");
-		var cmd = args[0].slice(cmdPrefix.length).toLowerCase();
+		var args = message.split(" ");
+		var cmd = args[0].slice(2).toLowerCase();
 		var query = args.slice(1).join(" ");
 
 		switch (cmd) {
 			case "help":
 			case "h":
-				msg.channel.send({embed:{
+				bot.sendMessage({embed:{
 					title: "Commands",
 					description:
 						"\n`p!screenshot <url>`"+
@@ -42,84 +37,81 @@ console.log("start");
 						"\n\n[» Invite](https://discordapp.com/oauth2/authorize?scope=bot&client_id=482784865532641290)"+
 						"\n[» Repository](https://github.com/ledlamp/puppeteer-discord-bot)"+
 						"\n[» Submit an issue](https://github.com/ledlamp/puppeteer-discord-bot/issues/new)"
-				}});
+				}, to: channelID});
 				break;
 			case "screenshot":
 			case "ss":
-				pup(msg, (query.startsWith("http://") || query.startsWith("https://")) ? query : `http://${query}`);
+				pup((query.startsWith("http://") || query.startsWith("https://")) ? query : `http://${query}`);
 				break;
 			case "google":
 			case "g":
-				pup(msg, `https://www.google.com/search?q=${encodeURIComponent(query)}`);
+				pup(`https://www.google.com/search?q=${encodeURIComponent(query)}`);
 				break;
 			case "google-images":
 			case "gi":
-				pup(msg, `https://www.google.com/search?q=${encodeURIComponent(query)}&tbm=isch&safe=${msg.channel.nsfw ? 'off' : 'on'}`);
+				pup(`https://www.google.com/search?q=${encodeURIComponent(query)}&tbm=isch&safe=${(bot.channels[channelID] && bot.channels[channelID].nsfw) ? 'off' : 'on'}`);
 				break;
 			case "bing":
 			case "b":
-				pup(msg, `https://www.bing.com/search?q=${encodeURIComponent(query)}`);
+				pup(`https://www.bing.com/search?q=${encodeURIComponent(query)}`);
 				break;
 			case "bing-images":
 			case "bi":
-				pup(msg, `https://www.bing.com/images/search?q=${encodeURIComponent(query)}&safeSearch=${msg.channel.nsfw ? 'off' : 'moderate'}`);
+				pup(`https://www.bing.com/images/search?q=${encodeURIComponent(query)}&safeSearch=${(bot.channels[channelID] && bot.channels[channelID].nsfw) ? 'off' : 'moderate'}`);
 				break;
 			case "youtube":
 			case "yt":
-				pup(msg, `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`);
+				pup(`https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`);
 				break;
 			case "ebay":
 			case "e":
-				pup(msg, `https://www.ebay.com/sch/i.html?_nkw=${encodeURIComponent(query)}`);
+				pup(`https://www.ebay.com/sch/i.html?_nkw=${encodeURIComponent(query)}`);
 				break;
 			case "amazon":
 			case "a":
-				pup(msg, `https://www.amazon.com/s?k=${encodeURIComponent(query)}`);
+				pup(`https://www.amazon.com/s?k=${encodeURIComponent(query)}`);
 				break;
 /*
 			case "":
 			case "":
-				pup(msg, ``);
+				pup(``);
 				break;
 */
 
 			case "eval":
 			case ">":
-				if (msg.author.id == "281134216115257344") {
+				if (userID == "281134216115257344") {
 					try {
-						msg.channel.send(eval(query));
+						bot.sendMessage({message: eval(query), to: channelID});
 					} catch(e) {
-						msg.channel.send(e);
+						bot.sendMessage({message: e, to: channelID});
 					}
 				}
 				break;
 		}
 
-
-	});
-
-	async function pup(message, url) {
-		message.react('🆗');
-		try {
-			var page = await browser.newPage();
-			page.on("error", error => {
-				message.channel.send(`:warning: ${error.message}`);
-			});
-			await page.setViewport({width: 1440, height: 900});
-			await page.goto(url);
-			var screenshot = await page.screenshot({type: 'png'});
-			await message.channel.send(new Discord.Attachment(screenshot, "screenshot.png"));
-		} catch(error) {
-			console.error(error);
-			await message.channel.send(`:warning: ${error.message}`);
-		} finally {
+		async function pup(url) {
+			bot.addReaction({reaction:'🆗', channelID, messageID: event.d.id});
 			try {
-				await page.close();
-			} catch(e) {
-				console.error(e);
-				process.exit(1);
+				var page = await browser.newPage();
+				page.on("error", error => {
+					bot.sendMessage({message: `:warning: ${error.message}`, to: channelID});
+				});
+				await page.setViewport({width: 1440, height: 900});
+				await page.goto(url);
+				var screenshot = await page.screenshot({type: 'png'});
+				bot.uploadFile({file: screenshot, filename: "screenshot.png", to: channelID});
+			} catch(error) {
+				console.error(error);
+				bot.sendMessage({message: `:warning: ${error.message}`, to: channelID});
+			} finally {
+				try {
+					await page.close();
+				} catch(e) {
+					console.error(e);
+					process.exit(1);
+				}
 			}
 		}
-	}
-
+	});
 })();
